6533ccb155882cace7d05b32da899b6671ad7e24
Sonali Konge <sonalikonge@vaal-triangle.com>
2020-02-13 12:43:23 +0530

    error solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const app = require('./app')
-const port = process.env.PORT
+//const port = process.env.PORT
 
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+// app.listen(port, () => {
+//     console.log('Server is up on port ' + port)
+// })
+
+var server = app.listen(process.env.PORT, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+  });
+  
